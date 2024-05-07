d81915c4884b819c1af71a2f53641f6b71c34307
Jdeaza78 <159087749+Jdeaza78@users.noreply.github.com>
2024-05-07 13:44:17 -0400

    created public folder and added the image as a static asset and index.html

---
+++ index.js
@@ -0,0 +1,29 @@
+// import express into our file
+const express = require('express')
+
+//Create a new express application
+const app = express()
+
+// define port our server will run on
+const port = 3000
+
+//allow static asset in public folder
+app.use(express.static('public'))
+
+//define our server routes
+app.get('/', (req, res)=>{
+    res.send("Server is operational")
+})
+
+app.get('/message', (req, res)=>{
+    //do something when the server processes this request
+    
+    //send back a response to the client
+    res.send("Server is operational")
+})
+
+// rum our server to listen at the port we defined
+
+app.listen(port, ()=>{
+    console.log(Server is running on port ${port})
+})
